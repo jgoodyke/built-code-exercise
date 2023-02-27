a27eec8e7e77e44d58c0c9ea016679d9c06623d6
Jason Goodyke <jgoodyke@users.noreply.github.com>
2023-02-27 14:08:19 -0600

    cleanup

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -5,15 +5,6 @@ import ViewBlogPosts from '../views/BlogPosts/ViewBlogPosts';
 const AppRouter: React.FC = () => (
     <Routes>
         <Route path="/" element={<ViewBlogPosts />} />
-
-        {/* <Route exact path={StatusPage.Unauthorized} component={Unauthorized401} />
-      <Route exact path={StatusPage.NotFound} component={NotFound404} />
-      <Route exact path={StatusPage.Error} component={Error500} />
-      <Route exact path="/home" component={HomeDomain} />
-      <SecureRoute path="/configurations" component={ConfigurationsDomain} allowedScopes={['read:rom_configurations']} />
-      <SecureRoute path="/route-rules" component={RouteRulesDomain} allowedScopes={['read:rom_route_rules']} />
-      <Route exact path="/" render={() => <Navigate to="/home" />} />
-      <Route render={() => <Navigate to={StatusPage.NotFound} />} /> */}
     </Routes>
 );
 
